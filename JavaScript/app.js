70b064fa0425c812ba72be1674f4223424f238b9
unknown <mohsen.rezaii@icloud.com>
2019-05-08 13:20:02 +0200

    i just ipload all i did from 1th of April

---
+++ JavaScript/app.js
@@ -0,0 +1,60 @@
+/*var todoList = {
+
+
+    todo:[
+        {
+            todoText: "Item 1",
+            completed: true
+        },
+        {
+            todoText: "Item 2",
+            completed: false
+        },
+        {
+            todoText: "Item 3",
+            completed: true
+        },
+
+    ],
+
+
+    showTodo: function() {
+        console.log(this.todo);
+    },
+
+
+    addTodo: function(texttodo) {
+        this.todo.push({
+            texttodo: texttodo,
+            completed: false
+        });
+        this.showTodo();
+    },
+
+
+    changeTodo: function(position, text) {
+        this.todo[position].todoText = text;
+        this.showTodo();
+    },
+
+
+    deleteTodo: function(position) {
+        this.todo.splice(position,1);
+        this.showTodo();
+    },
+
+
+    toggleCompleted: function(){
+        this.todo[position].completed = ! this.todo[position].completed;
+        this.showTodo();
+    },
+
+    toggleAll: function() {
+        for(var i = 0; i < this.todo.length; i++);
+        if (this.todo[i].completed === true) {
+
+        }
+
+    }
+};*/
+let email = 'nisar.ahmed.hatami@powercoders.org'
