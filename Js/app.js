80c5a625b1959b407df2b2447a6a252672409a18
unknown <mohsen.rezaii@icloud.com>
2019-05-17 14:53:58 +0200

    change the nav to hamburger menu to mobile view

---
+++ Js/app.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+
+
+    // HOWE TO ADD CALSSS TO UN ELEMENT USING HOVWR
+    $("").hover(function(){
+        $("").toggleClass("");
+    });
+
+
+    // REMOVE & ADDING CLASS TO UN ELEMENT WITH ONE CLICK
+    $("#open-menu").on("click", function(){
+        $("#mynav").toggleClass(" responsive");
+    });
+    
+    
---
+++ menu-twenty/assets/js/app-START.js
@@ -0,0 +1,13 @@
+/* WARNING !
+    THIS PAGE USES jQuery AND FILE FUNCTIONS "functions.js"
+    BE SURE TO INSERT "jQuery" and "functions.js" BEFORE USING THE SCRIPTS BELOW
+*/
+
+
+/**
+ * jQuery
+ * --------------------------------------------------------------
+ * On document ready, do this...
+ * Before using the code below you need to install jQuery!
+ */
+/*START jQuery Code*/
